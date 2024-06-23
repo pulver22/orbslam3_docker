aa9d0507651169da63b109e58878fa1ad358cab2
Riccardo <pulver22@gmail.com>
2024-06-23 16:49:10 +0200

    Added command for downloading BLT dataset

--- download_dataset_sample.sh
+++ download_dataset_sample.sh
@@ -1,1 +1,5 @@
+# Euroc
 wget http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_01_easy/MH_01_easy.zip -O Datasets/EuRoC/MH01.zip&&unzip Datasets/EuRoC/MH01.zip -d Datasets/EuRoC/MH01 && rm Datasets/EuRoC/MH01.zip
+
+# BACCHUS Long-Term
+wget -O Datasets/BLT.zip https://lcas.lincoln.ac.uk/nextcloud/index.php/s/9Jiti4kzpe5T3bC/download && unzip Datasets/BLT.zip -d Datasets/BLT && rm Datasets/BLT.zip
